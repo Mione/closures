48d41c5306a73f6ec1b9a23985876260a276af6a
Mio <ionescu.m@outlook.com>
2015-06-29 11:50:54 +0300

    master - finishing changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
-var myDiv = document.querySelectorAll('main div');
+'use strict';
+var myDiv = document.querySelectorAll('main div'),
+    counter = new Array(5);
 
-for(var i = 0;i<myDiv.length; i++) {
+
+
+
+function handler(i) {
+    return function () {
+        counter[i] = counter[i] + 1;
+        myDiv[i].innerHTML = "clicks: " + counter[i];
+    }
+}
+
+for (var i= 0; i < myDiv.length; i++) {
+  counter[i]= 0;
+  myDiv[i].innerHTML = "clicks: " + counter[i];
+  myDiv[i].addEventListener('click', handler(i));
+}
+
+
+/*(for(var i = 0;i<myDiv.length; i++) {
   (function (i){
     var counter = 0;
     myDiv[i].addEventListener('click', function () {counter = counter+1; console.log(counter)});
   })(i);
 }
+*/
